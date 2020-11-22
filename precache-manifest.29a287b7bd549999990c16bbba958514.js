896a634c1f52ac574f953c6acaf38a6c5a342c18
tyler belmonte <tylerbelmonte@gmail.com>
2020-11-22 14:30:59 -0600

    Updates

---
+++ precache-manifest.29a287b7bd549999990c16bbba958514.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d71b6cb445a6e1425e185652cfe44f",
+    "url": "/weatherapi/index.html"
+  },
+  {
+    "revision": "e03bb18d6e1aafb8d924",
+    "url": "/weatherapi/static/css/2.631c1da9.chunk.css"
+  },
+  {
+    "revision": "b63b641f63ad7eaa82f4",
+    "url": "/weatherapi/static/css/main.a0a9f39d.chunk.css"
+  },
+  {
+    "revision": "e03bb18d6e1aafb8d924",
+    "url": "/weatherapi/static/js/2.856cdf61.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weatherapi/static/js/2.856cdf61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b63b641f63ad7eaa82f4",
+    "url": "/weatherapi/static/js/main.9ca2eda1.chunk.js"
+  },
+  {
+    "revision": "12419fc5eae991bb9f77",
+    "url": "/weatherapi/static/js/runtime-main.af534d48.js"
+  },
+  {
+    "revision": "0c1381818eaf80e21180dcf93ad52df8",
+    "url": "/weatherapi/static/media/backroundimage.0c138181.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weatherapi/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weatherapi/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weatherapi/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weatherapi/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/weatherapi/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
